b159a6c28dd2d4fc4346fa024ea5fc7c1d3acfa0
SawaniX <tomekwalburg@wp.pl>
2024-01-07 22:29:32 +0100

    add models

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,15 +1,18 @@
 import os
 import re
-from torchvision.io import read_image
+import numpy as np
+from PIL import Image
 from torch.utils.data import Dataset
 
 
 class CustomDataset(Dataset):
-    def __init__(self, labels_path, samples_path, pattern=r'-\d+\.png'):
+    def __init__(self, labels_path, samples_path, pattern=r'-\d+\.png', transform=None, target_transform=None):
         self.labels_path = labels_path
         self.samples_path = samples_path
         self.samples_list = os.listdir(self.samples_path)
         self.pattern = pattern
+        self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self):
         return len(self.samples_list)
@@ -17,10 +20,15 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         sample_name = self.samples_list[idx]
         sample_path = os.path.join(self.samples_path, sample_name)
-        sample = read_image(sample_path)
+        sample = np.array(Image.open(sample_path))
 
         label_name = ''.join(re.split(self.pattern, sample_name)) + '.jpg'
         label_path = os.path.join(self.labels_path, label_name)
-        label = read_image(label_path)
+        label = np.array(Image.open(label_path))
+
+        if self.transform:
+            sample = self.transform(sample)
+        if self.target_transform:
+            label = self.target_transform(label)
 
         return sample, label
--- main.py
+++ main.py
@@ -1,18 +1,95 @@
 import os
-from torch.utils.data import DataLoader, random_split
+import torch
+import numpy as np
 from dataset import LoadData
+from models import Discriminator, Generator
+from torchvision.transforms import ToTensor
+from torch import nn, optim
+from tqdm import tqdm
+from urils import save_checkpoint, load_checkpoint, save_some_examples
 
 import matplotlib.pyplot as plt
 
-
-train_dataloader, test_dataloader = LoadData().get_dataloader(train_split=0.7,
-                                                              test_split=0.3,
-                                                              batch_size=64)
+torch.backends.cudnn.benchmark = True
 
 
+def train_epoch(discriminator, generator, loader, opt_disc, opt_gen, L1, BCE, g_scaler, d_scaler, device, LAMBDA):
+    loop = tqdm(loader, leave=True)
 
-_, axs = plt.subplots(1, 2)
-for X, y in train_dataloader:
-    axs[0].imshow(X[0].T)
-    axs[1].imshow(y[0].T)
-    plt.show()
+    for idx, (x, y) in enumerate(loop):
+        x, y = x.to(device), y.to(device)
+
+        with torch.cuda.amp.autocast_mode.autocast():
+            y_fake = generator(x)
+            D_real = discriminator(x, y)
+            D_fake = discriminator(x, y_fake.detach())
+            D_real_loss = BCE(D_real, torch.ones_like(D_real))
+            D_fake_loss = BCE(D_fake, torch.zeros_like(D_fake))
+            D_loss = (D_real_loss + D_fake_loss) / 2
+
+        discriminator.zero_grad()
+        d_scaler.scale(D_loss).backward()
+        d_scaler.step(opt_disc)
+        d_scaler.update()
+
+        with torch.cuda.amp.autocast_mode.autocast():
+            D_fake = discriminator(x, y_fake)
+            G_fake_loss = BCE(D_fake, torch.ones_like(D_fake))
+            l1 = L1(y_fake, y) * LAMBDA
+            G_loss = G_fake_loss + l1
+
+        opt_gen.zero_grad()
+        g_scaler.scale(G_loss).backward()
+        g_scaler.step(opt_gen)
+        g_scaler.update()
+
+def main():
+    LEARNING_RATE = 0.002
+    NUM_EPOCH = 100
+    SAVE_MODEL = True
+    LAMBDA = 100
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+
+    discriminator = Discriminator(in_channels=3).to(device)
+    generator = Generator(in_channels=3).to(device)
+    opt_disc = optim.Adam(discriminator.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+    opt_gen = optim.Adam(generator.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+    BCE = nn.BCEWithLogitsLoss()
+    L1 = nn.L1Loss()
+
+    train_dataloader, test_dataloader = LoadData(transform=ToTensor(),
+                                                target_transform=ToTensor()
+                                                ).get_dataloader(train_split=0.7,
+                                                                test_split=0.3,
+                                                                batch_size=1)
+    g_scaler = torch.cuda.amp.grad_scaler.GradScaler()
+    d_scaler = torch.cuda.amp.grad_scaler.GradScaler()
+
+    print('============= TRAINING STARTED =============')
+    for epoch in range(NUM_EPOCH):
+        print(epoch)
+        train_epoch(discriminator, generator, train_dataloader, opt_disc, opt_gen, L1, BCE, g_scaler, d_scaler, device, LAMBDA)
+
+        if SAVE_MODEL and epoch % 5 == 0:
+            save_checkpoint(generator, opt_gen, filename='gen.pth.tar')
+            save_checkpoint(discriminator, opt_disc, filename='disc.pth.tar')
+
+        save_some_examples(generator, test_dataloader, epoch, folder="evaluation", device=device)
+
+
+    # _, axs = plt.subplots(1, 2)
+    # for X, y in train_dataloader:
+    #     axs[0].imshow(torch.permute(X[0], (1, 2, 0)))
+    #     axs[1].imshow(torch.permute(y[0], (1, 2, 0)))
+    #     plt.show()
+
+    
+if __name__=='__main__':
+    main()
