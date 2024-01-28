c6051441f2abff61f63c66e28917837ef697af48
SawaniX <tomekwalburg@wp.pl>
2024-01-28 21:18:07 +0100

    move training stats to tensorboard

--- models/trainer.py
+++ models/trainer.py
@@ -4,7 +4,8 @@ from torch.utils.data import DataLoader
 from torch import nn
 from torch.optim import Adam
 from tqdm import tqdm
-from torchvision.utils import save_image
+from torchvision.utils import make_grid
+from torch.utils.tensorboard import SummaryWriter
 
 
 class Trainer:
@@ -20,6 +21,7 @@ class Trainer:
         self.discriminator_optimizer: Adam = Adam(self.discriminator.parameters(), 
                                                   lr=config.get('LEARNING_RATE'), 
                                                   betas=config.get('BETAS'))
+        self.writer = SummaryWriter()
         
     def train(self, train_dataloader: DataLoader, test_dataloader: DataLoader):
         BCE = nn.BCEWithLogitsLoss()
@@ -37,7 +39,7 @@ class Trainer:
                 D_fake = self.discriminator(x, y_fake.detach())
                 D_real_loss = BCE(D_real, torch.ones_like(D_real))
                 D_fake_loss = BCE(D_fake, torch.zeros_like(D_fake))
-                D_loss = D_real_loss + D_fake_loss
+                D_loss = (D_real_loss + D_fake_loss) / 2
 
                 self.discriminator.zero_grad()
                 D_loss.backward()
@@ -45,8 +47,8 @@ class Trainer:
 
                 D_fake = self.discriminator(x, y_fake)
                 G_fake_loss = BCE(D_fake, torch.ones_like(D_fake))
-                l1 = L1(y_fake, y) * config.get('LAMBDA')
-                G_loss = G_fake_loss + l1
+                l1 = L1(y_fake, y)
+                G_loss = G_fake_loss + l1 * config.get('LAMBDA')
 
                 self.generator_optimizer.zero_grad()
                 G_loss.backward()
@@ -54,21 +56,19 @@ class Trainer:
 
             if config.get('SAVE_MODEL') and epoch % 5 == 0:
                 self._save_checkpoint()
-
-            self._save_example(test_dataloader, epoch)
+                self._save_example(test_dataloader, epoch)
+            self.writer.add_scalar('Discriminator loss', D_loss, epoch)
+            self.writer.add_scalar('Generator loss', G_loss, epoch)
 
     def _save_example(self, test_dataloader: DataLoader, epoch: int) -> None:
-        folder = 'evaluation'
         x, y = next(iter(test_dataloader))
         x, y = x.to(self.device), y.to(self.device)
         self.generator.eval()
         with torch.no_grad():
             y_fake = self.generator(x)
             y_fake = y_fake * 0.5 + 0.5
-            save_image(y_fake, folder + f"/y_gen_{epoch}.png")
-            save_image(x, folder + f"/input_{epoch}.png")
-            if epoch == 1:
-                save_image(y, folder + f"/label_{epoch}.png")
+            img_grid = make_grid([x.squeeze(0), y.squeeze(0), y_fake.squeeze(0)])
+            self.writer.add_image('Prediction', img_grid, epoch)
         self.generator.train()
         
     def _save_checkpoint(self) -> None:
@@ -79,7 +79,7 @@ class Trainer:
             'discriminator_optimizer_state_dict': self.discriminator_optimizer.state_dict(),
             }, self.SAVE_PATH)
         
-    def load_checkpoint(self, eval: bool = True, lr: float = None) -> None:
+    def load_checkpoint(self, lr: float = None) -> None:
         checkpoint = torch.load(self.SAVE_PATH)
         self.generator.load_state_dict(checkpoint.get('generator_state_dict'))
         self.discriminator.load_state_dict(checkpoint.get('generator_optimizer_state_dict'))
@@ -91,9 +91,5 @@ class Trainer:
             for gen_param_group, disc_param_group in zip(self.generator_optimizer.param_groups, self.discriminator_optimizer.param_groups):
                 gen_param_group['lr'] = lr
                 disc_param_group['lr'] = lr
-        if eval:
-            self.generator.eval()
-            self.discriminator.eval()
-        else:
-            self.generator.train()
-            self.discriminator.train()
+        self.generator.train()
+        self.discriminator.train()
